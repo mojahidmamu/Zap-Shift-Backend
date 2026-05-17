d223e6b2033deca2cdc048d47dcef89ddbf8380b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-05-17 22:09:35 +0600

    added

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,7 +3,12 @@ const admin = require('firebase-admin');
 
 // You need a service account key from Firebase Console
 // Go to Project Settings > Service Accounts > Generate New Private Key
-const serviceAccount = require('./zapshift-secret.json');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
